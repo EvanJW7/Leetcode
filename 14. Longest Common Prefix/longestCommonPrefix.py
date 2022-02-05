bdf5624e991a6012b8073f5c395edcc3569e4c2f
Evan Wright <84414002+EvanJW7@users.noreply.github.com>
2022-02-05 15:01:51 -0600

    Add files via upload

--- 14. Longest Common Prefix/longestCommonPrefix.py
+++ 14. Longest Common Prefix/longestCommonPrefix.py
@@ -1,16 +1,23 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        output = ""
-        
+        #Take care of a couple edge cases to speed up run time 
+        if len(strs) == 0:
+            return ""
         if len(strs) == 1:
-            return ''.join(strs)
+            return strs[0]
         
-        for i in range(len(strs[0])):
-            for s in strs:
-                if i == len(s) or s[i] != strs[0][i]:
-                    return output
-            output += strs[0][i]
-        return output
+        #Make an empty prefix to add to later
+        prefix = ""
+        first_word = strs[0]
         
-
-
+        for i in range(len(first_word)):
+            for word in strs:
+                #If it is out of bounds or not equal to the same letter, return what prefix we have
+                if i == len(word) or word[i] != first_word[i]:
+                    return prefix
+            #If that is not the case, continue to build the prefix 
+            prefix += word[i]
+        
+        return prefix 
+    
+    
