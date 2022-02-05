cb76068afe8be7783bdd77b5285aeb3c3b68efd6
Evan Wright <84414002+EvanJW7@users.noreply.github.com>
2022-02-05 17:41:49 -0600

    Add files via upload

---
+++ 231. Power of Two/powerofTwo.c
@@ -0,0 +1,15 @@
+bool isPowerOfTwo(int n){
+    //Take care of edge case, if the number is less than or equal to 0 return false
+    if (n <= 0) return false;
+    
+    //If not, return not n&n-1
+    return !(n&(n-1));
+    
+    //Explanation:
+    //In binary, all powers of two will only have one 1 in them. 
+    //For example, 4 is represented as 0100. 
+    //0100 -1 flips the digits to 1011
+    //0100 & 1011 == 0000 due to propositional logic
+    //!0000 = 1111 == true, else false
+
+}
