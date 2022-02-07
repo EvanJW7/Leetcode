a9f146bc891b50da26c8a824046c4cb7344224ab
Evan Wright <84414002+EvanJW7@users.noreply.github.com>
2022-02-07 13:25:17 -0600

    Update maximumSubarray.java

--- 53. Maximum Subarray/maximumSubarray.java
+++ 53. Maximum Subarray/maximumSubarray.java
@@ -2,6 +2,8 @@ class Solution {
     public int maxSubArray(int[] nums) {
         //Make your two variables max_sum and current_sum
         int max_sum = nums[0];
+        //Max sum has to be the first number in the array, not 0, because what would happen if they array is a singular negative number?
+        //Current sum can 0 or anything below that
         int current_sum = 0;
         
         //Iterate through the nums array, use Kadane's algorithm to find the max sum 
