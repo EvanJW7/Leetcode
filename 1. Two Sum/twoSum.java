c3cb5d52c48ef29a98b98ea059270bff8a913f39
Evan Wright <84414002+EvanJW7@users.noreply.github.com>
2022-02-11 20:14:56 -0600

    Update twoSum.java

--- 1. Two Sum/twoSum.java
+++ 1. Two Sum/twoSum.java
@@ -1,17 +1,18 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         //Make a hashmap to store key, value pairs
-        Map<Integer, Integer> seen = new HashMap<>();
+        Map<Integer, Integer> seen = new HashMap();
         
         //Logic - loop throught the nums list, if we have seen a value that equals to target return the indexes
-        //Otherwise, continue to build the hashmap 
+        //Otherwise, continue to build the hashmap
         for (int i = 0; i<nums.length; i++){
-            if (seen.containsKey(target - nums[i])) {
-                return new int[] {seen.get(target-nums[i]), i};
+            int complement = target-nums[i];
+            if (seen.containsKey(complement)) {
+                return new int[] {seen.get(complement), i};
             }
             seen.put(nums[i], i);
         }
-        //Throw an error so you will always have a return statement 
+        //Throw an error in case there is no match
         throw new IllegalArgumentException("no match");
     }
 }
