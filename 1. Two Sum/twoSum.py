3da5e677dac4fb65e637eeb3d50e429a729a0440
Evan Wright <84414002+EvanJW7@users.noreply.github.com>
2022-03-15 19:25:44 -0500

    Update twoSum.py

--- 1. Two Sum/twoSum.py
+++ 1. Two Sum/twoSum.py
@@ -1,6 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
+        '''
+        Logic - we want to make a seen dictionary and solve the problem by the time the seen dictionary is complete. We do not build a dictionary
+        first and then loop through it, we can find a solution while in the process of building the dictionary. We want to loop through each num in 
+        nums and check if the target-num is in our dictionary, if it is, we have a paring and we can return their indexes. If not, add it to the 
+        dictionary. Because a solution is guaranteed, we can solve it this way. 
+        '''
+        
         #Make an empty dictionary
         seen = {}
         
@@ -9,7 +16,7 @@ class Solution:
             #If the target-num in seen, return the index of target-num and the index of the number you are on
             if target-num in seen:
                 return [seen[target-num], i]
-            #If num from the numlist is not in seen, update the dictionary with the num and its index
-            elif num not in seen:
+            #If not, add to dictionary
+            else:
                 seen[num] = i
                 
