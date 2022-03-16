d4db6ab0642ad211c64bce6235d8e54f9ab38619
Evan Wright <84414002+EvanJW7@users.noreply.github.com>
2022-03-15 19:26:18 -0500

    Update twoSum.py

--- 1. Two Sum/twoSum.py
+++ 1. Two Sum/twoSum.py
@@ -4,8 +4,8 @@ class Solution:
         '''
         Logic - we want to make a seen dictionary and solve the problem by the time the seen dictionary is complete. We do not build a dictionary
         first and then loop through it, we can find a solution while in the process of building the dictionary. We want to loop through each num in 
-        nums and check if the target-num is in our dictionary, if it is, we have a paring and we can return their indexes. If not, add it to the 
-        dictionary. Because a solution is guaranteed, we can solve it this way. 
+        nums and check if the target-num is in our dictionary. If it is, we have a paring and we can return their indexes. If not, add it to the 
+        dictionary.
         '''
         
         #Make an empty dictionary
