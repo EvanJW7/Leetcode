d7f28b3b8866bfc1f787be8684660dd5304a2e13
Evan Wright <84414002+EvanJW7@users.noreply.github.com>
2022-03-18 12:03:31 -0500

    Update buyStock.py

--- 121. Best Time to Buy and Sell Stock/buyStock.py
+++ 121. Best Time to Buy and Sell Stock/buyStock.py
@@ -5,8 +5,12 @@ class Solution:
         rp = 1
         maxProfit = 0
         
-        #While the roght pointer is less than the length of prices
+        #While the right pointer is less than the length of prices
         while rp<len(prices):
+            '''
+            Logic - we are basically finding a low and checking the max profit it can make until the end 
+            of the array. If we find a new low, we update the left pointer and continue to check maxProfit
+            '''
             #If the trade is profitable, check it against the max profit and update if needed
             if prices[lp] < prices[rp]:
                 maxProfit = max(maxProfit, prices[rp]-prices[lp])
