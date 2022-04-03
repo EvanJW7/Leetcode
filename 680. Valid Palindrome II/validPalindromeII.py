6f2e44d2383ee7d09f765d47ab51a7ac784559df
Evan Wright <84414002+EvanJW7@users.noreply.github.com>
2022-04-03 15:24:06 -0500

    Add files via upload

--- 680. Valid Palindrome II/validPalindromeII.py
+++ 680. Valid Palindrome II/validPalindromeII.py
@@ -1,17 +1,21 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        if s[::] == s[::-1]:
-            return True 
-        else:
-            if len(s)%2 != 0 and s[::] != s[::-1]:
-                 return False 
-            if len(s)%2 == 0:
-                one_counts = []
-                for letter in s:
-                    if s.count(letter) == 1:
-                        one_counts.append(letter)
-                        s = s.replace(letter, '')
-                        if s[::] == s[::-1]:
-                            return True 
-                        return False 
+        #Initialize your left and right pointers
+        lp = 0
+        rp = len(s)-1
+        
+        #While they do not cross, check to see if the letter at each pointer is equal. If so, increment
+        while lp<rp:
+            if s[lp] == s[rp]:
+                lp += 1
+                rp -= 1
+            #If the letters at both pointers are not equal, check to see if the subarray of either the 
+            #left letter being skipped or the right letter being skipped is a palindrome. If yes, we
+            #can return true
+            else:
+                skipL = s[lp + 1: rp+1]
+                skipR = s[lp:rp]
+                return skipL == skipL[::-1] or skipR == skipR[::-1]
+           
+        return True 
             
