f2514cc823cbe32205563501b612efd4bbb3ce2a
Evan Wright <84414002+EvanJW7@users.noreply.github.com>
2022-09-09 19:16:25 -0500

    Update singleNumber.java

--- 136. Single Number/singleNumber.java
+++ 136. Single Number/singleNumber.java
@@ -2,7 +2,7 @@ class Solution {
     public int singleNumber(int[] nums) {
         
         int result = 0;
-        //We can use BitOr to find the number than appears only once
+        //We can use Bit-Or to find the number than appears only once
         for (int i = 0; i<nums.length; i++){
             result = result ^ nums[i];
         }
